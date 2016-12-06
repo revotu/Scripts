122214825423392606d5b684dd0a992aaa82daeb
revotu <632624460@qq.com>
2016-12-06 14:53:13 +0800

    Fixed youtube video link check

--- checkVideoLink.py
+++ checkVideoLink.py
@@ -4,15 +4,19 @@ import urllib
 import openpyxl
 from openpyxl.reader.excel import load_workbook
 
-def checkVideoLink(videoLink):
-    videoID = videoLink.split('/')[-1]
-    videoThumbnail = 'https://i.ytimg.com/vi/%s/maxresdefault.jpg' % (videoID)
-    response = urllib.urlopen(videoThumbnail)
+def youtubeChecker(videoID):
+    checkUrl = 'http://www.youtube.com/oembed?url=http://www.youtube.com/watch?v=%s' % (videoID)
+    response = urllib.urlopen(checkUrl)
     status = response.getcode()
     if status == 200:
         return True
     else:
         return False
+
+
+def checkVideoLink(videoLink):
+    videoID = videoLink.split('/')[-1]
+    return youtubeChecker(videoID)
 
 if __name__ == "__main__":
     wb = load_workbook('enki-video.xlsx')
@@ -22,3 +26,12 @@ if __name__ == "__main__":
         ws.cell(row = index,column = 3).value = checkVideoLink(videoLink)
         print index,videoLink
         wb.save('enki-video.xlsx')
+#     wb = load_workbook('enki-video.xlsx')
+#     ws = wb.active
+#     f = open('enki-video-error-link.txt',a)
+#     for index in range(1,ws.get_highest_row() + 1):
+#         status = ws.cell(row = index,column = 3).value
+#         link = ws.cell(row = index,column = 1).value
+#         if status == 'False':
+#             f.write('http://www.enkivillage.com/%s.html' % (link))
+#     f.close()
